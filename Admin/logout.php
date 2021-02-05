e5343630bdbfa1e57212fc0b6b4abf8f6a5dba0f
priyanshudutta21 <priyanshudutta775@gmail.com>
2021-02-05 19:31:32 +0530

    jnklnl

--- Admin/logout.php
+++ Admin/logout.php
@@ -4,6 +4,6 @@ session_start();
 session_unset();
 session_destroy();
 
-header("LOCATION: /index.php");
+header("LOCATION: /index.html");
 
 ?>
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# HMS
+# HMS hhh
 
 [![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)]
 
